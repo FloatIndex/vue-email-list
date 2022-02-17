8a78467134a288be0e19e2768e2d948e5980c7da
Marianna Galuppi <marianna.galuppi@gmail.com>
2022-02-17 20:04:14 +0100

    css commit

--- main.js
+++ main.js
@@ -2,7 +2,15 @@ const app = new Vue (
     {
         el: '#app',
         data: {
-            
+            emailAddresses: []
+        },
+        mounted() {
+            for(let i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                    this.emailAddresses.push(response.data.response);
+                });
+            }
         }
     }
 )
